866206a5e7713b2a17080c0ae2aea53f8a800984
QToxan <anton.gusv@yandex.ua>
2018-06-30 19:55:11 +0300

    add get_roman and get_arabic

---
+++ ConvertRA/function.py
@@ -0,0 +1,54 @@
+
+CONVERT = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000}
+def get_arabic(roman_number):
+    max = 1
+    result = 0
+    roman_number = roman_number[-1::-1]
+
+    for sign in roman_number:
+        if CONVERT[sign] >= max:
+            result += CONVERT[sign]
+            max = CONVERT[sign]
+        else:
+            result -= CONVERT[sign]
+
+    if(get_roman(result) == roman_number[-1::-1]):
+        return result
+    else:
+        return "Bad input"
+
+
+
+def get_roman(arabic_number):
+    '''Возвращает правильные значения в интервале 1...3999'''
+    result = ''
+    group = [[1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1],
+            ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]]   
+    number = int(arabic_number)
+
+    if number < 1 or number > 3999:
+        return "Bad input"
+
+    for i in range(len(group[0])):
+        j = 0
+        while j < number // group[0][i]:
+            result += group[1][i]
+            j += 1
+        number %= group[0][i]
+    return result
+
+ARABIC = set('0123456789')
+ROMAN = set('IVXLCDM')
+def convert_number(instr):
+    in_set = set(str(instr))
+    res = 0
+    if (len(in_set) == 0 ):
+        res = ''
+    elif (in_set <= ARABIC):
+        res = get_roman(instr)
+    elif(in_set <= ROMAN):
+        res = get_arabic(instr)
+    else:
+        res = "Error: Недопустимые символы"
+
+    return res
---
+++ ConvertRA/main.py
@@ -0,0 +1,8 @@
+from function import *
+
+def main():
+    text = input()
+    print(convert_number(text))
+
+if __name__ == "__main__":
+    main()
